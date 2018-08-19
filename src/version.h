5aa8c75fed72e9a7e1f7bedcfa7324990606d60b
GiantCoreDev <dev@giantpay.network>
2018-08-20 03:34:24 +0700

    * set last pow block value to 71400 * increment protocol version * increase maturity to 200 * set budget cycle value to 1 month

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70715;
+static const int PROTOCOL_VERSION = 70716;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,8 +22,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70714;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70715;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70716;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70716;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
