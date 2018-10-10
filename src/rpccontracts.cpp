b0c35f40fbda4f65ef445c1a30fb6ce28d80b920
mdimon <m91s0@yandex.ru>
2018-10-11 01:55:39 +0700

    upgrade smart-contract methods

--- src/rpccontracts.cpp
+++ src/rpccontracts.cpp
@@ -2,6 +2,7 @@
 
 #include "base58.h"
 #include "rpcserver.h"
+#include "net.h"
 
 #include <boost/assign/list_of.hpp>
 
@@ -10,22 +11,22 @@ using namespace boost::assign;
 using namespace json_spirit;
 using namespace std;
 
-Value ContractDeploy(const CBitcoinAddress& address, string code, double prioritet)
+Value ContractDeploy(const CBitcoinAddress& addressObject, string jscode, const double prioritet)
 {
   return 0;
 }
 
-Value ContractCall(bool readonly, double prioritet,const CBitcoinAddress& address, string methodName, Array args)
+Value ContractCall(bool readonly, const double& prioritet,const CBitcoinAddress& addressObject, const string& methodName, const Array& args)
 {
   return 0;
 }
 
-Value ContractAddress(string strHex)
+Value ContractAddress(const string& strHex)
 {
     return 0;
 }
 
-Value ContractInfo(const CBitcoinAddress& address, bool calculateProperties, bool callReadonlyMethods)
+Value ContractInfo(const CBitcoinAddress& addressObject, bool calculateProperties, bool callReadonlyMethods)
 {
     return 0;
 }
@@ -45,87 +46,142 @@ Value ContractStatus(const CBitcoinAddress& address)
     return 0;
 }
 
-Value ContractDeployEstimate(string code, double prioritet, uint64_t loopCount)
+Value ContractDeployEstimate(string jscode, const double& prioritet, uint64_t loopCount)
 {
     return 0;
 }
 
-Value ContractCallEstimate(const CBitcoinAddress& address, const string& methodName, double prioritet, uint64_t loopCount)
+Value ContractCallEstimate(const CBitcoinAddress& addressObject, const string& methodName, const double& prioritet, uint64_t loopCount)
 {
     return 0;
 }
 
-Value ContractCallEstimate()
+Value ContractCount()
 {
     return 0;
 }
 
 Value contractdeploy(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 1 || params.size() > 3)
+    if (fHelp || params.size() != 1)
         throw runtime_error(
-            "contractdeploy \"address\" \"code\" \"prioritet:\"\n"
-            "\nArguments:\n"
-            "\naddress - (string) public key through which indication the private key for the signature of transaction will be chosen\n"
-            "\ncode - (string) smart contract code\n"
-            "\nprioritet - (double) the weight influencing the size of the commission burned for warm the smart contract"
-            "\nCreates transaction which deploit the smart contract and sends it to mempool\n"
-            "\nResult:\n"
-            "\n    if transaction was added to mempool - that the method will return a transaction hash\n"
-            "\nExamples:\n" +
-            HelpExampleCli("contractdeploy", "") + HelpExampleRpc("contractdeploy", ""));
+                "contractdeploy \"address\" \"jscode\" \"prioritet:\"\n"
+                "\nCreates transaction which deploit the smart contract and sends it to mempool\n"
+                "\nArguments:\n"
+                "       {\n"
+                "         \"address\":\"address\", - (string) public key through which indication the private key for the signature of transaction will be chosen\n"
+                "         \"jscode\":\"jscode\", - (string) smart contract code\n"
+                "         \"prioritet\": prioritet - (double) the weight influencing the size of the commission burned for warm the smart contract\n"
+                "       }\n"
+                "\nResult:\n"
+                "\n    if transaction was added to mempool - that the method will return a transaction hash\n"
+                "       }\n"
+                "    ]\n"
+                );
 
-    RPCTypeCheck(params, list_of(str_type)(str_type)(real_type));
+    RPCTypeCheck(params, list_of(obj_type));
 
-    CBitcoinAddress address(params[0].get_str());
-    if (bool isValid = address.IsValid())
+    Object deployInfo = params[0].get_obj();
+    string address;
+    string jscode;
+    double prioritet = 1;
+
+    BOOST_FOREACH(const Pair& s, deployInfo) {
+        if (s.name_ == "address") {
+            address = s.value_.get_str();
+        } else if (s.name_ == "jscode") {
+            jscode = s.value_.get_str();
+        } else if (s.name_ == "prioritet") {
+            prioritet = s.value_.get_real();
+        }
+    };
+
+    if (address.empty()) {
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
+    CBitcoinAddress addressObject(address);
+    if (!addressObject.IsValid()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
 
-    std::string code = params[1].get_str();
+    if (jscode.empty()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid JScode");
+    }
 
-    double prioritet = params[2].get_real();
-    if (prioritet < 1.0)
-        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong prioritet");
+    if (prioritet < 1) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong parametr prioritet");
+    }
 
-    return ContractDeploy(address, code, prioritet);
+    return ContractDeploy(addressObject, jscode, prioritet);
 }
 
 Value contractcall(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 1 || params.size() > 5)
+    if (fHelp || params.size() != 1)
         throw runtime_error(
             "contractcall \"readonly\" \"prioritet:\" \"address\" \"methodName\" \"args\" \n"
             "\nArguments:\n"
-            "\nreadonly - (bool: true|false) if readonly=true and in a call of a method goes\n"
-            "             attempt that to change in a blockchain (for example to change a variable\n"
-            "             in the smart contract) there will be a mistake\n"
-            "\nprioritet - (double) the weight influencing the size of the commission burned for warm the smart contract"
-            "\naddress - (string) public key through which indication the private key for the signature of transaction will be chosen\n"
-            "\nmethodName - (string) name of a method\n"
-            "\nargs - (array) method call parameter"
+            "       {\n"
+            "         \"readonly\": true/false,- (bool: true|false) if readonly=true and in a call of a method goes\n"
+            "                                    attempt that to change in a blockchain (for example to change a variable\n"
+            "                                    in the smart contract) there will be a mistake\n"
+            "         \"prioritet\": prioritet, - (double) the weight influencing the size of the commission burned for warm the smart contract"
+            "         \"address\": \"address\", - (string) public key through which indication the private key for the signature of transaction will be chosen\n"
+            "         \"methodName\": \"methodName\", - (string) name of a method\n"
+            "         \"args\": args - (array) method call parameter"
+            "       }\n"
             "\nResult:\n"
             "\nif the readonly=true method that returns value which will return a smart contract method\n"
             "\nif the readonly=false method and transaction it is successfully added that the transaction hash will return\n"
             "\nExamples:\n" +
             HelpExampleCli("contractcall", "") + HelpExampleRpc("contractcall", ""));
 
-    RPCTypeCheck(params, list_of(bool_type)(real_type)(str_type)(str_type)(array_type));
+    RPCTypeCheck(params, list_of(obj_type));
 
-    bool readonly = params[0].get_bool();
+    Object callInfo = params[0].get_obj();
+    bool readonly;
+    double prioritet = 1;
+    string address;
+    string methodName;
+    Array args;
 
-    double prioritet = params[1].get_real();
-    if (prioritet < 1.0)
-        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong prioritet");
+    BOOST_FOREACH(const Pair& c, callInfo) {
+        if (c.name_ == "readonly") {
+            readonly = c.value_.get_bool();
+        } else if (c.name_ == "prioritet") {
+            prioritet = c.value_.get_real();
+        } else if (c.name_ == "address") {
+            address = c.value_.get_str();
+        } else if (c.name_ == "prioritet") {
+            prioritet = c.value_.get_real();
+        } else if (c.name_ == "methodName") {
+            methodName = c.value_.get_str();
+        } else if (c.name_ == "args") {
+            args = c.value_.get_array();
+        }
+    };
 
-    CBitcoinAddress address(params[2].get_str());
-    if (bool isValid = address.IsValid())
+    if (prioritet < 1) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong parametr prioritet");
+    }
+
+    if (address.empty()) {
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
+    CBitcoinAddress addressObject(address);
+    if (!addressObject.IsValid()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
 
-    string methodName = params[3].get_str();
+    if (methodName.empty()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong methodName parametr");
+    }
 
-    Array args = params[4].get_array();
+    if (args.empty()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong args parametr");
+    }
 
-    return ContractCall(readonly, prioritet, address, methodName, args);
+    return ContractCall(readonly, prioritet, addressObject, methodName, args);
 }
 
 Value contractaddress(const Array& params, bool fHelp)
@@ -150,30 +206,47 @@ Value contractaddress(const Array& params, bool fHelp)
 
 Value contractinfo(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 1 || params.size() > 3)
+    if (fHelp || params.size() != 1)
         throw runtime_error(
             "contractinfo \"address\" \"calculateProperties\" \"callReadonlyMethods\" \n"
             "\nArguments:\n"
-            "\naddress - (string) public key through which indication the private key for the signature of transaction will be chosen\n"
-            "\ncalculateProperties - (bool) if true that are calculated values of all fields. If that false isn't specified\n"
-            "\ncallReadonlyMethods - (bool) if true that are calculated values of all readonly of methods. If that false isn't specified\n"
+            "       {\n"
+            "         \"address\": \"address\", - (string) public key through which indication the private key for the signature of transaction will be chosen\n"
+            "         \"calculateProperties\": true, - (bool) if true that are calculated values of all fields. If that false isn't specified\n"
+            "         \"callReadonlyMethods\": true - (bool) if true that are calculated values of all readonly of methods. If that false isn't specified\n"
+            "       }\n"
             "\nReturns information on the smart contract on his address\n"
             "\nResult:\n"
             "\ndata on the smart contract\n"
             "\nExamples:\n" +
             HelpExampleCli("contractinfo", "") + HelpExampleRpc("contractinfo", ""));
 
-    RPCTypeCheck(params, list_of(str_type)(bool_type)(bool_type));
+    RPCTypeCheck(params, list_of(obj_type));
 
-    CBitcoinAddress address(params[0].get_str());
-    if (bool isValid = address.IsValid())
+    Object contInfo = params[0].get_obj();
+    string address;
+    bool calculateProperties = false;
+    bool callReadonlyMethods = false;
+
+    BOOST_FOREACH(const Pair& a, contInfo) {
+        if (a.name_ == "address") {
+            address = a.value_.get_str();
+        } else if (a.name_ == "calculateProperties") {
+            calculateProperties = a.value_.get_bool();
+        } else if (a.name_ == "callReadonlyMethods") {
+            callReadonlyMethods = a.value_.get_bool();
+        }
+    };
+
+    if (address.empty()) {
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
+    CBitcoinAddress addressObject(address);
+    if (!addressObject.IsValid()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
 
-    bool calculateProperties = params[1].get_bool();
-
-    bool callReadonlyMethods = params[2].get_bool();
-
-    return ContractInfo( address, calculateProperties, callReadonlyMethods);
+    return ContractInfo( addressObject, calculateProperties, callReadonlyMethods);
 }
 
 Value contractcode(const Array& params, bool fHelp)
@@ -192,7 +265,7 @@ Value contractcode(const Array& params, bool fHelp)
     RPCTypeCheck(params, list_of(str_type));
 
     CBitcoinAddress address(params[0].get_str());
-    if (bool isValid = address.IsValid())
+    if (bool isValid = !(address.IsValid()))
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
 
     return ContractCode(address);
@@ -214,7 +287,7 @@ Value contractbalance(const Array& params, bool fHelp)
     RPCTypeCheck(params, list_of(str_type));
 
     CBitcoinAddress address(params[0].get_str());
-    if (bool isValid = address.IsValid())
+    if (bool isValid = !(address.IsValid()))
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
 
     return ContractBalance(address);
@@ -236,7 +309,7 @@ Value contractstatus(const Array& params, bool fHelp)
     RPCTypeCheck(params, list_of(str_type));
 
     CBitcoinAddress address(params[0].get_str());
-    if (bool isValid = address.IsValid())
+    if (bool isValid = !(address.IsValid()))
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
 
     return ContractStatus(address);
@@ -244,66 +317,115 @@ Value contractstatus(const Array& params, bool fHelp)
 
 Value contractdeployestimate(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 1 || params.size() > 3)
+    if (fHelp || params.size() != 1)
         throw runtime_error(
             "contractdeployestimate \"code\" \"prioritet:\" \"loopCount\"\n"
             "\nArguments:\n"
-            "\ncode - (string) smart contract code\n"
-            "\nprioritet - (double) the weight influencing the size of the commission burned for warm the smart contract"
-            "\nloopCount (uint64_t) quantity of cycles for calculation of the commission, by default value 1"
+            "       {\n"
+            "         \"jscode\": \"jscode\", - (string) smart contract code\n"
+            "         \"prioritet\": prioritet, - (double) the weight influencing the size of the commission burned for warm the smart contract"
+            "         \"loopCount\": loopCount (uint64_t) quantity of cycles for calculation of the commission, by default value 1"
+            "       }\n"
             "\nEstimates smart contract deploy cost\n"
             "\nResult:\n"
             "\nvalue of the commission and plan of implementation of the smart contract\n"
             "\nExamples:\n" +
             HelpExampleCli("contractdeployestimate", "") + HelpExampleRpc("contractdeployestimate", ""));
 
-    RPCTypeCheck(params, list_of(str_type)(real_type)(int_type));
+    RPCTypeCheck(params, list_of(obj_type));
 
-    string code = params[0].get_str();
+    Object deployestimateInfo = params[0].get_obj();
+    string jscode;
+    double prioritet = 1;
+    uint64_t loopCount =1;
 
-    double prioritet = params[1].get_real();
-    if (prioritet < 1.0)
-        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong prioritet");
+    BOOST_FOREACH(const Pair& b, deployestimateInfo) {
+        if (b.name_ == "jscode") {
+            jscode = b.value_.get_str();
+        } else if (b.name_ == "prioritet") {
+            prioritet = b.value_.get_real();
+        } else if (b.name_ == "loopCount") {
+            loopCount = b.value_.get_uint64();
+        }
+    };
 
-    uint64_t loopCount = params[2].get_uint64();
+    if (jscode.empty()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid JScode");
+    }
 
-    return ContractDeployEstimate(code, prioritet, loopCount);
+    if (prioritet < 1) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong parametr prioritet");
+    }
+
+    if (loopCount < 1) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong parametr loopCount");
+    }
+
+    return ContractDeployEstimate(jscode, prioritet, loopCount);
 }
 
 Value contractcallestimate(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 1 || params.size() > 4)
+    if (fHelp || params.size() != 1)
         throw runtime_error(
             "contractcallestimate \"address\" \"methodName\" \"prioritet:\" \"loopCount\"\n"
             "\nArguments:\n"
-            "\naddress - (string) the contract address, the line containing the valid address of the smart contract\n"
-            "\nmethodName - (string) smart contract method"
-            "\nprioritet - (double) the weight influencing the size of the commission burned for warm the smart contract"
-            "\nloopCount (uint64_t) quantity of cycles for calculation of the commission, by default value 1"
+            "       {\n"
+            "         \"address\": \"address\", - (string) the contract address, the line containing the valid address of the smart contract\n"
+            "         \"methodName\": \"methodName\", - (string) smart contract method"
+            "         \"prioritet\": prioritet, - (double) the weight influencing the size of the commission burned for warm the smart contract"
+            "         \"loopCount\": loopCount (uint64_t) quantity of cycles for calculation of the commission, by default value 1"
+            "       }\n"
             "\nEstimates the smart contract method call cost (not readonly)\n"
             "\nResult:\n"
             "\nvalue of the commission and plan of performance of a method of the smart contract\n"
             "\nExamples:\n" +
             HelpExampleCli("contractcallestimate", "") + HelpExampleRpc("contractcallestimate", ""));
 
-    RPCTypeCheck(params, list_of(str_type)(str_type)(real_type)(int_type));
+    RPCTypeCheck(params, list_of(obj_type));
 
-    CBitcoinAddress address(params[0].get_str());
-    if (bool isValid = address.IsValid())
+    Object callestimateInfo = params[0].get_obj();
+    string address;
+    string methodName;
+    double prioritet = 1;
+    uint64_t loopCount =1;
+
+    BOOST_FOREACH(const Pair& d, callestimateInfo) {
+        if (d.name_ == "address") {
+            address = d.value_.get_str();
+        } else if (d.name_ == "methodName") {
+            methodName = d.value_.get_str();
+        } else if (d.name_ == "prioritet") {
+            prioritet = d.value_.get_real();
+        } else if (d.name_ == "loopCount") {
+            loopCount = d.value_.get_uint64();
+        }
+    };
+
+    if (address.empty()) {
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
+    CBitcoinAddress addressObject(address);
+    if (!addressObject.IsValid()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Invalid address transaction");
+    }
 
-    string methodName = params[1].get_str();
+    if (methodName.empty()) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong methodName parametr");
+    }
 
-    double prioritet = params[2].get_real();
-    if (prioritet < 1.0)
-        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong prioritet");
+    if (prioritet < 1) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong parametr prioritet");
+    }
 
-    uint64_t loopCount = params[3].get_uint64();
+    if (loopCount < 1) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Wrong parametr loopCount");
+    }
 
-    return ContractCallEstimate(address, methodName, prioritet, loopCount);
+    return ContractCallEstimate(addressObject, methodName, prioritet, loopCount);
 }
 
 Value contractcount(const Array& params, bool fHelp)
 {
-    return ContractCallEstimate();
+    return ContractCount();
 }
