9d8e95b357e9d210220ef415b840186d3446ccd8
Dmitry Filinberg <filinberg@gmail.com>
2019-03-13 11:48:56 +0200

    v8 version updated to last stable version

--- depends/packages/libv8.mk
+++ depends/packages/libv8.mk
@@ -1,8 +1,9 @@
 PACKAGE=libv8
-$(package)_version_version=6.9.427.19
+$(package)_version_version=7.3.492
 $(package)_download_path=https://github.com/GiantPay/depot_tools/archive
-$(package)_file_name=1.0.0.tar.gz
-$(package)_sha256_hash=fa3a646f764d3605d19af5c001039cf820976cef962ccca59951fde95ff2556f
+$(package)_file_name=1.0.1.tar.gz
+$(package)_sha256_hash=2350f88081ebe367b73636ef654fe41be4765fe0a01a820c02aaa82dfa92e329
+$(package)_commit=9df9418edf64d6a080ba569ef43cd5ca0171a820
 
 define $(package)_preprocess_cmds
   export PATH="${PATH}:$($(package)_build_dir)"	&& \
@@ -10,25 +11,27 @@ define $(package)_preprocess_cmds
   cd $($(package)_build_dir)/v8 && \
   $($(package)_build_dir)/fetch v8 && \
   cd $($(package)_build_dir)/v8/v8 && \
+  $($(package)_build_dir)/v8/v8/tools/release/mergeinfo.py $($(package)_commit) && \
+  git checkout $($(package)_commit) && \
   $($(package)_build_dir)/gclient sync --with_branch_heads && \
-  $($(package)_build_dir)/gclient fetch && \
-  $($(package)_build_dir)/v8/v8/tools/release/mergeinfo.py aae279118869ad4ae1f31513a8b66a17ff2f0dd4 && \
-  git checkout aae279118869ad4ae1f31513a8b66a17ff2f0dd4
+  $($(package)_build_dir)/gclient fetch
 endef
 
 define $(package)_config_cmds
   cd $($(package)_build_dir)/v8/v8 && \
   $($(package)_build_dir)/v8/v8/tools/dev/v8gen.py x64.release -- \
     v8_monolithic=true \
+    is_component_build=false \
     v8_use_external_startup_data=false \
     v8_enable_i18n_support=false \
+    icu_use_data_file=false \
     use_custom_libcxx=false \
-    use_custom_libcxx_for_host=false
+    use_custom_libcxx_for_host=false \
+    v8_enable_test_features=false
 endef
 
 define $(package)_build_cmds
-  cd $($(package)_build_dir)/v8/v8 && \
-  $($(package)_build_dir)/ninja -C out.gn/x64.release
+  $($(package)_build_dir)/ninja -C $($(package)_build_dir)/v8/v8/out.gn/x64.release
 endef
 
 define $(package)_stage_cmds
