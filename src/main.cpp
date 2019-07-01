0a7ae71e3a5a9e6eaf9f4e3fb9bdc75282a3a995
Dmitry Filinberg <filinberg@gmail.com>
2019-07-02 00:52:06 +0200

    * magic phrase updated * version of protocol updated

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ static void CheckBlockIndex();
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "DarkNet Signed Message:\n";
+const string strMessageMagic = "GIANT Signed Message:\n";
 
 // Internal stuff
 namespace
--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70719;
+static const int PROTOCOL_VERSION = 70720;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,7 +22,7 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70718;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70719;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70720;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 70719;
